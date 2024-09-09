a7537acee6ebdc42329e8008341840f3865be4d7
sgtudeveloper <webdeveloper.igcsm@gmail.com>
2024-09-09 18:46:37 +0530

    correct the page name

--- components/FormPage.js
+++ components/FormPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Table1 from '@/components/Table'
+import Page from '@/components/Table'
 
 
 const FormPage = () => {
   return (
     <div className="p-8 pt-12 w-2/3 mx-auto">
     <h1 className='text-center text-3xl font-bold mb-6'>Enquiry Form Data</h1>
-    <Table1/>
+    <Page/>
     </div>
   );
 }
